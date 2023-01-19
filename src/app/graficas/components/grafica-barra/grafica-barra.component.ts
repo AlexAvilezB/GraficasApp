0620aa602df87506b4f960a7ff0c79f2da02d9aa
Alejandro Avilez <aleantonio656@gmail.com>
2023-01-18 19:28:34 -0600

    Componente de graficas dobles

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,11 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+
+import DataLabelsPlugin from 'chartjs-plugin-datalabels';
 
 @Component({
   selector: 'app-grafica-barra',
   templateUrl: './grafica-barra.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class GraficaBarraComponent {
+export class GraficaBarraComponent implements OnInit {
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
 
+  @Input() horizontal: boolean = false;
+  @Input() barChartData!: ChartData<'bar'>;
+
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    // We use these empty structures as placeholders for dynamic theming.
+    scales: {
+      x: {},
+      y: {
+        min: 0,
+        max: 100,
+      },
+    },
+    plugins: {
+      legend: {
+        display: true,
+      },
+      datalabels: {
+        anchor: 'end',
+        align: 'end',
+      },
+    },
+  };
+
+  public barChartType: ChartType = 'bar';
+  public barChartPlugins = [DataLabelsPlugin];
+
+  ngOnInit(): void {
+    if (this.horizontal) {
+      this.barChartOptions!.indexAxis = 'y';
+      this.barChartOptions!.scales!['y']!.min = 0;
+    }
+  }
+
+  public chartHovered({
+    event,
+    active,
+  }: {
+    event?: ChartEvent;
+    active?: {}[];
+  }): void {
+    console.log(event, active);
+  }
 }
